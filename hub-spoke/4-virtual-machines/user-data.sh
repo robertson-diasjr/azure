5a1986419463a763f7b87f3d9bdbfef234a56fea
robertson-diasjr <robertson.diasjr@gmail.com>
2022-01-08 00:12:01 -0300

    Added SSL on Azure VMs

--- hub-spoke/4-virtual-machines/user-data.sh
+++ hub-spoke/4-virtual-machines/user-data.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 sudo apt-get install -y apache2
-sudo systemctl start apache2
+sudo a2enmod ssl
+sudo a2ensite default-ssl.conf
 sudo systemctl enable apache2
-echo "<h1>Demo</h1>" | sudo tee /var/www/html/index.html
+export hostname=`hostname`
+echo "<h1>Demo on $hostname</h1>" | sudo tee /var/www/html/index.html
+sudo systemctl restart apache2
